7d468c4c62c6668af6a0b0b067046436b815e615
Christian Rackerseder <git@echooff.de>
2016-08-05 17:52:50 +0200

    Added rollup-plugin-uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'src/dater.js',
   format: 'cjs',
-  plugins: [ babel() ],
+  plugins: [ babel(), uglify() ],
   dest: 'dist/bundle.js'
 };
