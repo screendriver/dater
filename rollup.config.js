7d9ca8edd73207ef0a1c8e3fefad9350e053ec8e
Christian Rackerseder <git@echooff.de>
2016-08-07 07:50:44 +0200

    Added rollup-plugin-filesize

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
+import filesize from 'rollup-plugin-filesize';
 import json from 'rollup-plugin-json';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'src/dater.js',
   format: 'cjs',
-  plugins: [ json(), babel(), uglify() ],
+  plugins: [ json(), babel(), uglify(), filesize() ],
   dest: 'dist/bundle.js'
 };
