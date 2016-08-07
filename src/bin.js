c2d83a031c5320b53a1e78a9ce4885cff2acf2ca
Christian Rackerseder <git@echooff.de>
2016-08-07 09:18:14 +0200

    Removed rollupjs

--- src/bin.js
+++ src/bin.js
@@ -1,7 +1,13 @@
+#!/usr/bin/env node
+
+import 'babel-polyfill';
 import program from 'commander';
 
+// import rename from './dater';
 import { version } from '../package.json';
 
 program
   .version(version)
   .parse(process.argv);
+
+console.log(version);
