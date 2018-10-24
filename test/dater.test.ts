e63d61722affecc30c4b289748c5795a62b7826e
Christian Rackerseder <git@echooff.de>
2018-10-24 21:04:29 +0200

    ♻️ Change file extension to *.ts

--- test/dater.test.ts
+++ test/dater.test.ts
@@ -2,7 +2,7 @@ import test from 'ava';
 
 import rename from '../src/redater';
 
-test('should fail on invalid path', async (t) => {
+test('should fail on invalid path', async t => {
   const { code } = await t.throws(rename('./foo'));
   t.is(code, 'ENOENT');
 });
