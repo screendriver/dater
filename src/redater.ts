928d8e89e4801ae1341a14d0911a59b78982df67
Christian Rackerseder <git@echooff.de>
2019-07-27 20:30:05 +0200

    📥 Use image and directory module

--- src/redater.ts
+++ src/redater.ts
@@ -1,69 +1,11 @@
 import fs from 'fs-extra';
-import mime from 'mime';
 import moment from 'moment';
 import path from 'path';
-import { ExifImage } from 'exif';
 import { Ora } from 'ora';
+import { readExifDate, NoExifDate, ExifDate } from './image';
+import { getImages } from './directory';
 
-interface NoExifDate {
-  ok: boolean;
-}
-
-interface ExifDate {
-  ok: boolean;
-  createDate: string;
-  filePath: string;
-}
-
-function getImages(dirPath: string): Promise<readonly string[]> {
-  return new Promise((resolve, reject) => {
-    fs.readdir(dirPath, (err, files) => {
-      if (err) {
-        reject(err);
-      } else {
-        const images = files.filter(file => {
-          const type = mime.getType(file);
-          if (!type) {
-            return false;
-          }
-          return type.startsWith('image');
-        });
-        resolve(images);
-      }
-    });
-  });
-}
-
-function readExifDate(image: string, spinner?: Ora) {
-  return new Promise<NoExifDate | ExifDate>((resolve, reject) => {
-    // tslint:disable-next-line
-    new ExifImage(
-      { image },
-      (error: { code: string }, exifData: { exif: { CreateDate: string } }) => {
-        if (error) {
-          if (error.code === 'NO_EXIF_SEGMENT') {
-            if (spinner) {
-              const spinnerInstance = spinner;
-              spinnerInstance.text = `No EXIF data found in ${image}`;
-              spinnerInstance.stopAndPersist();
-            }
-            resolve({ ok: false });
-          } else {
-            reject(error);
-          }
-        } else {
-          resolve({
-            ok: true,
-            createDate: exifData.exif.CreateDate,
-            filePath: image,
-          });
-        }
-      },
-    );
-  });
-}
-
-export async function rename(dirPath: string, spinner?: Ora) {
+export async function rename(dirPath: string, spinner: Ora) {
   const exifReads: Array<Promise<NoExifDate | ExifDate>> = [];
   const files = await getImages(dirPath);
   for (const file of files) {
