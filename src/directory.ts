7dae13ae0a923d88ec2b565365d96f94d53a1c6c
Christian Rackerseder <git@echooff.de>
2019-07-28 19:32:15 +0200

    ✨ Create more directory related functions

--- src/directory.ts
+++ src/directory.ts
@@ -1,12 +1,20 @@
 import fs from 'fs-extra';
 import mime from 'mime';
+import path from 'path';
 
 type Path = string;
 
-export async function getImages(dirPath: string): Promise<readonly Path[]> {
-  const files = await fs.readdir(dirPath);
+export function getAllFiles(dirPath: Path): Promise<readonly Path[]> {
+  return fs.readdir(dirPath);
+}
+
+export function filterImages(files: readonly Path[]): readonly Path[] {
   return files.filter(file => {
     const type = mime.getType(file);
     return type && type.startsWith('image');
   });
+}
+
+export function resolveImageFullPath(dirPath: Path, imageName: string): Path {
+  return path.resolve(dirPath, imageName);
 }
--- test/directory.test.ts
+++ test/directory.test.ts
@@ -1,9 +1,29 @@
 import test from 'tape';
-import { getImages } from '../src/directory';
+import path from 'path';
+import {
+  getAllFiles,
+  filterImages,
+  resolveImageFullPath,
+} from '../src/directory';
 
-test('getImages() filters images in directory path', async t => {
+test('getAllFiles() returns all files from given directory path', async t => {
   t.plan(1);
-  const actual = await getImages('./test');
-  const expected = ['test-image-exif.jpg', 'test-image.jpg'];
+  const actual = await getAllFiles('./test/assets');
+  const expected = ['test-image-exif.jpg', 'test-image.jpg', 'text-only.txt'];
   t.deepEqual(actual, expected);
+});
+
+test('filterImages() filters all images from given files', t => {
+  t.plan(1);
+  const files = ['./test/assets/test-image.jpg', './test/assets/text-only.txt'];
+  const actual = filterImages(files);
+  const expected = ['./test/assets/test-image.jpg'];
+  t.deepEqual(actual, expected);
+});
+
+test('resolveImageFullPath() returns the absolute path of the given image', t => {
+  t.plan(1);
+  const actual = resolveImageFullPath('./test/assets', 'test-image.jpg');
+  const expected = path.resolve('./test/assets', 'test-image.jpg');
+  t.equal(actual, expected);
 });
